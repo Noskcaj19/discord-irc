25256ba4ba22054f59b2321c164e9afc0063e7f6
Noskcaj <kcajdev@gmail.com>
2018-05-25 17:49:43 -0400

    Load discord token

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "`DISCORD_TOKEN` not set, exiting")
+		os.Exit(1)
+	}
+
 	server := NewServer("nos")
 	server.Run()
 }
